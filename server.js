053dd4ca929d181f5eb65741514b3434223d38b2
Alal Uddin <alalu75666@gmail.com>
2023-03-16 08:30:04 +0530

    json-server-deployment

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const PORT = 8080;  // You can change port from here
-const path = require('path');
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'db.json'));  // This is where you can change your file path
-const middlewares = jsonServer.defaults();
-server.use(middlewares);
-server.use(jsonServer.bodyParser);
-server.use('/', router);
-server.listen(PORT, () => console.log(`JSON Server is running on http://localhost:${PORT}`))
+const PORT = process.env.PORT || 8080
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`)
+})
